54ec85033d7cbacbbc9a8cce2165777c4878ada5
Abian004 <121283182+Abian004@users.noreply.github.com>
2022-12-30 15:39:22 +0700

    Percobaan 1 Abian

--- Projects/MainProject.cpp
+++ Projects/MainProject.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main()
 {   cout << "Hello World"; //Pratama
-
+	cout<<"Industrial Society and Its Consecuence by Theodore John Kaczynsky"<<endl; //Abian
     return 0;
 }
