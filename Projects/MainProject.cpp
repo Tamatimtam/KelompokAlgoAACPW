6c6ee81cd5d5266304344742fef65e5e1aaaa677
bangjur <azzuriptr@gmail.com>
2022-12-30 15:57:22 +0700

    punya juri

--- Projects/MainProject.cpp
+++ Projects/MainProject.cpp
@@ -5,5 +5,6 @@ int main()
 {   cout << "Hello World"; //Pratama
 	cout << "TIK melaju, kita bangkit, kita solid"; //Christian
 	cout<<"Industrial Society and Its Consecuence by Theodore John Kaczynsky"<<endl; //Abian
+	cout<<" diberitahukan diberi diberitarahukan"; //Azzuri
     return 0;
 }
