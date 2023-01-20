0c7bda0a1ccfb01a5ee30718978d9590e9991833
unknown <pratama.varian.andika.parulian.tik22@mhsw.pnj.ac.id>
2023-01-20 14:50:12 +0700

    Test Push

--- Projects/MainProject.cpp
+++ Projects/MainProject.cpp
@@ -14,9 +14,7 @@ const int N = 50; // number of books
 // and outputs a message indicating whether the book was found or not
 // By pratama
 void searchBuku(string Bukus[], string key) {
-    
-    //!!!WIP BELOM MASUK FLOWCHART!!! -Pratama
-    
+        bool found = false;
     // convert the key to lowercase using std::transform and ::tolower
     std::transform(key.begin(), key.end(), key.begin(), ::tolower);
     // convert all of the book names in the array to lowercase 
@@ -24,9 +22,9 @@ void searchBuku(string Bukus[], string key) {
         std::transform(Bukus[i].begin(), Bukus[i].end(), Bukus[i].begin(), ::tolower);
     }//to-lowercase function by Pratama
 
-    bool found = false;
+
     //linear search contributed by Azzuri
-    //loop through all books within database, to see if it matches the key -azzuri
+    //loop through all books within database, t o see if it matches the key -azzuri
     for (int i = 0; i < N; i++) {
         if (Bukus[i] == key) {
             found = true;
@@ -34,7 +32,7 @@ void searchBuku(string Bukus[], string key) {
             break;
         }
     }
-
+  
     //if found, declare that the book title is available -azzuri
     if (found) {
         cout << "--------------------------\n"; // Wahyu
@@ -66,21 +64,9 @@ int main() {
     "Negeri Para Bedebah","Perahu Kertas","Percy Jackson and the Olympians", "Pergi", "Pulang", "Pride and Prejudice", "Ready Player One", 
     "Robohnya Surau Kami","Sang Pemimpi", "Sapiens", "Selection", "Sherlock Holmes", "Supernova","The Hound of the Baskervilles",
     "The Memoirs of Sherlock Holmes","The Shawsank Redemption","Think and Grow Rich", "To Kill a Mockingbird", "Twilight"};
-
-    // Opening Banner - Wahyu
-    cout << "====================================================================================\n";
-    cout << "| <<<<<<<<<<<<<<<<<<<<<<<<<<< WELCOME TO BukuKita APP >>>>>>>>>>>>>>>>>>>>>>>>>>>> |\n";
-    cout << "====================================================================================\n";
-    cout << "| Bingung mau cari buku apa? Cari di BukuKita aja, dijamin lengkap dan berkualitas |\n";
-    cout << "====================================================================================\n\n";
-
-    // ask for student NIM -azzuri
-    string nim;
-    cout << "[Login] Enter student NIM: ";
-    cin >> nim;
     
-    // list array of NIMs and Names -azzuri
-    bool found = false;
+     // list array of NIMs and Names -azzuri
+    
     string a[] = {"2207421031","2207421032","2207421033","2207421034","2207421035","2207421036","2207421037","2207421038",
     "2207421039","2207421040","2207421041","2207421042","2207421043","2207421044","2207421045","2207421046","2207421047",
     "2207421048","2207421049","2207421050","2207421051","2207421052","2207421053","2207421054","2207421055","2207421056",
@@ -93,6 +79,21 @@ int main() {
   "Abdurrahman Ammar Ihsan","Wahyu Priambodo","Salsabilla Aulia","Izzaturachmi","Jonathan Victorian Wijaya","Fachrul Rosi","Rizki Alfarisi","Theo Dhiya Pratama",
   "Muhammad Haikal Fadhillah","Muhammad Brian Azura Nixon","Shaquille Ariza Hidayat","Safina Ayu Rasya","Cornelius Yuli Rosdianto"};
    //list of names  -Azzuri
+
+    // Opening Banner - Wahyu
+    cout << "====================================================================================\n";
+    cout << "| <<<<<<<<<<<<<<<<<<<<<<<<<<< WELCOME TO BukuKita APP >>>>>>>>>>>>>>>>>>>>>>>>>>>> |\n";
+    cout << "====================================================================================\n";
+    cout << "| Bingung mau cari buku apa? Cari di BukuKita aja, dijamin lengkap dan berkualitas |\n";
+    cout << "====================================================================================\n\n";
+
+    // ask for student NIM -azzuri
+    string nim;
+    cout << "[Login] Enter student NIM: ";
+    cin >> nim;
+    bool found = false;
+    
+   
 
     //loop through nim input and see if nim is in the list -pratama
     for (int i = 0; i < 29; i++) {
@@ -126,12 +127,12 @@ int main() {
                     getline(cin,key);
                     cout << endl;
 
-                    searchBuku(Bukus, key);
+                    searchBuku(Bukus, key); //pratama
                     
                     // ask if user wants to search for another Book -wahyu
                     cout << "[Confirm] Search for another book? (y/n): ";
                     done = !getYesNo();
-                    cout << endl;
+                    cout << endl; 
                 }
                 break;
             }
@@ -183,7 +184,7 @@ int main() {
             }
         }
         cout << "-------------\n";
-        cout << "[ THANK YOU ]\n";
+        cout << "[ THANK YOU ]\ n";
         cout << "-------------\n";
 
        
